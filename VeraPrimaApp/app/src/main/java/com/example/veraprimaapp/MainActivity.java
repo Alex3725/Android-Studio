f3e8c9a92642633fa33b8fbe0acba92b4eb68782
ALESSANDRO PREVITALI <previtali.alessandro.08@itisdalmine.edu.it>
2026-04-28 13:41:12 +0200

    login aura

--- VeraPrimaApp/app/src/main/java/com/example/veraprimaapp/MainActivity.java
+++ VeraPrimaApp/app/src/main/java/com/example/veraprimaapp/MainActivity.java
@@ -1,20 +1,27 @@
 package com.example.veraprimaapp;
 
+import android.annotation.SuppressLint;
 import android.graphics.Color;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button btn;
-    TextView text1;
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +32,54 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        this.btn = findViewById(R.id.btn1);
-        this.text1 = findViewById(R.id.textView1);
-        final boolean[] isClick = {false};
+
+
+        Button btn = findViewById(R.id.btn1);
+        TextView text1 = findViewById(R.id.textView1);
+        EditText email = findViewById(R.id.editTextEmail);
+        EditText password = findViewById(R.id.editTextPassword);
+
+
+
+        final String[] account = {"alex@gmail.com", "1234"};
+        boolean[] isLogged = {false};
         btn.setOnClickListener(new View.OnClickListener() {
+
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
+                String em = email.getText().toString().trim();
+                String psw = password.getText().toString().trim();
 
-                text1.setText(isClick[0] ? "ho spento" : "Ho cliccato");
-                text1.setBackgroundColor(getColor(isClick[0] ? R.color.blue : R.color.red));
+                if (em.isEmpty()) {
+                    email.setError("Email obbligatoria");
+                    return;
+                }
 
-                isClick[0] = !isClick[0];
+                if (psw.isEmpty()) {
+                    password.setError("Password obbligatoria");
+                    return;
+                }
+
+
+                if (em == account[0] && psw == account[1]){
+                    text1.setText(getString(R.string.error_login));
+                    isLogged[0] = true;
+                }
+                    // OK login
+
+                    text1.setBackgroundColor(
+                            ContextCompat.getColor(MainActivity.this, (isLogged[0] ? R.color.blue : R.color.red))
+                    );
+                    text1.setTextColor(
+                            Color.WHITE
+                    );
+                    text1.setText(getString((isLogged[0] ? R.string.succesfull_login : R.string.error_login)));
+                    Toast.makeText(v.getContext(), "Login OK", Toast.LENGTH_SHORT).show();
             }
         });
+
+
+
     }
 }
